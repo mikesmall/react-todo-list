8c80255cd22ec19fcaa2ba05b7f37cdf6be6b0b8
mikesmall <michael.l.a.small@gmail.com>
2017-12-12 15:12:21 -0500

    Creates ListItem.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import MyList from './MyList';
+import './index.css';
 
 var toDos = ["Buy ice cream", "Eat ice cream", "Go to the gym"]
 
-
 ReactDOM.render(
-  <MyList theList={toDos} />, 
+  <MyList theList={toDos} />,
   document.getElementById('root')
 );
